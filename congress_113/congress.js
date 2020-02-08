a10f8491ca168b28db1f521d30ceb9de6b77e73a
Harris Richardson <harris.richardson87@gmail.com>
2020-02-08 17:23:36 +0100

    Now with Vue

--- congress_113/congress.js
+++ congress_113/congress.js
@@ -3,6 +3,15 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const senateUrl = "http://api.propublica.org/congress/v1/113/senate/members.json";
 const houseUrl = "http://api.propublica.org/congress/v1/113/house/members.json";
 let url;
+let data;
+let loaderTwo;
+var app = new Vue({
+  el: '#two',
+  data: {
+    rowDataTwo: []
+  }
+
+})
 
 if (location.href.includes("senate")) {
   url = senateUrl
@@ -23,22 +32,23 @@ function filterFunction() {
     throw new Error(res.statusText);
 
   }).then(function (json) {
-    let data = json;
-    stater(data.results[0].members)
+    data = json;
+    stater(data.results[0].members);
     return data;
 
   }).then(function (data) {
     checker(data);
 
+
   }).catch(function (error) {
     console.log("request failed:" + error.message);
   });
 }
+
 filterFunction();
 
-
-
 function checker(data) {
+  console.log(data, "checker front")
   let democratListen = document.getElementById("democrat");
   let republicanListen = document.getElementById("republican");
   let independentListen = document.getElementById("independent");
@@ -55,9 +65,16 @@ function checker(data) {
       if (republicanListen.checked && member.party == 'R' && member.state == stateSelect) return member;
       if (independentListen.checked && member.party == 'I' && member.state == stateSelect) return member;
     }
+
   })
-  pageLoader(result)
-}
+  console.log(result, "checker back")
+
+  app.rowDataTwo = result
+
+
+
+
+};
 
 const stater = (stacked) => {
   let stateBox = [];
@@ -78,50 +95,4 @@ const stater = (stacked) => {
     states.setAttribute('id', `${sortBox[i]}`)
     document.getElementById("state_select").appendChild(states);
   }
-}
-//TBODY Page Loader//
-
-const pageLoader = (stacked) => {
-  var Table = document.getElementById("tbody");
-  Table.innerHTML = "";
-
-  for (let i = 0; i < stacked.length; i++) {
-    let row = document.createElement('TR'); // Create row
-    row.setAttribute('scope', 'row')
-    row.setAttribute("class", `${stacked[i].state}`)
-    row.setAttribute('id', `${stacked[i].party}`)
-
-    let node = document.createElement("TD");
-    let textname = document.createTextNode(`${stacked[i].first_name}  ${stacked[i].last_name}`); // Create a text node
-    node.appendChild(textname);
-    node.setAttribute("id", `${stacked[i].last_name}`)
-    row.appendChild(node);
-
-    let nodetwo = document.createElement("TD");
-    let textparty = document.createTextNode(`${stacked[i].party}`); // Create a text nodetwo
-    nodetwo.appendChild(textparty);
-    nodetwo.setAttribute("id", `${stacked[i].party}`)
-    row.appendChild(nodetwo);
-
-    let nodethree = document.createElement("TD");
-    let textstate = document.createTextNode(`${stacked[i].state}`); // Create a text nodethree
-    nodethree.appendChild(textstate);
-    row.appendChild(nodethree)
-
-    let nodefour = document.createElement("TD");
-    let textsenor = document.createTextNode(`${stacked[i].seniority}`); // Create a text nodefour
-    nodefour.appendChild(textsenor);
-    nodefour.setAttribute("id", `${stacked[i].seniority}`)
-
-    row.appendChild(nodefour)
-
-    let nodefive = document.createElement("TD");
-    let textpercentage = document.createTextNode(`${stacked[i].votes_with_party_pct}`); // Create a text nodefour
-    nodefive.appendChild(textpercentage);
-    nodefive.setAttribute("id", `${stacked[i].votes_with_party_pct}`)
-
-    row.appendChild(nodefive)
-    document.getElementById("tbody").appendChild(row);
-
-  }
-}
+};
